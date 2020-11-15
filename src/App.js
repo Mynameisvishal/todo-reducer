4dfe431f3c0e040247f33d425997a685a6efa3b8
Vishal P <vishalprakaash@github.com>
2020-11-15 14:50:01 +0530

    user reducer used

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState, useReducer } from 'react';
 import './App.css';
+import Todo from './Todo';
 
-function App() {
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  DELETE_TODO: 'delete-todo',
+  TRIGGER_TODO: 'trigger-todo'
+}
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TRIGGER_TODO:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return {...todo,complete: !todo.complete }
+        }
+        return todo
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id)
+    default:
+      return todos
+  }
+}
+
+function newTodo(name) {
+  return {id: Date.now(),name:name,comeplete:false}
+}
+
+export default function App() {
+  const [todos,dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('');
+
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To do list</h1>
+      <form  onSubmit={onSubmit}>
+        <input type="text" onChange={(e)=>setName(e.target.value)} value={name}/>
+      </form>
+      {todos.map(todo => {  
+        return <Todo todo={todo} dispatch={dispatch}/>
+      })}
     </div>
   );
 }
 
-export default App;
